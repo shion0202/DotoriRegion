4058595aef5dc98240ce004932906dce9ba8f2fc
Verebell <verebell2020@gmail.com>
2023-12-03 02:42:28 +0900

    Init repository
    Init repository.

---
+++ DotoriRegion/Source/DotoriRegion/DotoriRegionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DotoriRegionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOTORIREGION_API ADotoriRegionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
